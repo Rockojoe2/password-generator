9814043793a6689fac9aefc7b68fc3285b01d4da
Rockojoe2 <lee.d.joey@gmail.com>
2023-06-28 15:00:19 -0500

    Rewrote the while loop to have 16 different paths. Also fixed the all false path to work and get the box to close

--- script.js
+++ script.js
@@ -24,10 +24,10 @@
 var generateBtn = document.querySelector("#generate");
 
 //Below are every character possible that my password can use.
-const lowerCaseLetterBank = "abcdefghijklmnopqrstuvwxyz";
-const upperCaseLetterBank = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numericalNumbersBank = "0123456789";
-const specialCharactersBank = "!@#$%^&*()_+~[]|<>/\-=";
+const lowerCaseLetterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCaseLetterBank = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numericalNumbersBank = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharactersBank = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "[", "]", "|", "<", ">", "/"];
 
 //Function generatePassword
 function generatePassword(){
@@ -39,6 +39,26 @@ function generatePassword(){
   var upperCaseLetters = window.confirm("Include uppercase?"); //True or false statement
   var numericalNumbers = window.confirm("Include numerical numbers?"); //True or false statement
   var specialCharacters = window.confirm("Include special characters?"); //True or false statement
+
+  /*16 Outcomes that can happen (lowerCase, upperCase, numerical, special)
+  
+  1. T T T T done
+  2. T F T T done
+  3. T T F T done
+  4. T F F T done
+  5. T T T F done
+  6. T F T F done
+  7. T T F F done
+  8. T F F F done
+  9. F T T T done
+  10. F F T T done
+  11. F T F T done
+  12. F F F T done
+  13. F T T F done
+  14. F F T F done
+  15. F T F F done
+  16. F F F F done
+  */ 
 
 
   var randomPasswordGenerated = ""; //Empty password to keep adding onto. Length here will be 0.
@@ -53,31 +73,112 @@ function generatePassword(){
   // console.log("Response 5: " + specialCharacters);
  
   // Below while statement will generate the password based on choices the user made
-  while(intPasswordLength > randomPasswordGenerated.length)
+  //TODO - Currently the random password is generated with the pattern of lower case, upper case, number, special character. Need to make it completely random.
+  //Could maybe come up with a word bank for every scenario and an if statement for every scenario, but that would be too long I think. There has to be a more efficient way. 
+  //Create an option string before while loop. Use concat of the two variables. Example of how concat looks - var galaxy = constellations.concat(planets); var galaxy = new variable and constellations and planets were the previous variables.
+
+  var key = true;
+  while(intPasswordLength > randomPasswordGenerated.length && key === true)
   {
-    if(lowerCaseLetters == true){
-      randomPasswordGenerated += lowerCaseLetterBank[Math.floor(Math.random() * lowerCaseLetterBank.length)]; //Will add lower case if user clicked okay
+    var combinedPassword = "";
+    //1
+    if(lowerCaseLetters == true && upperCaseLetters == true && numericalNumbers == true && specialCharacters == true){
+      combinedPasswordBank = lowerCaseLetterBank.concat(upperCaseLetterBank, numericalNumbersBank, specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
     }
 
-    if(upperCaseLetters == true){
-      randomPasswordGenerated += upperCaseLetterBank[Math.floor(Math.random() * upperCaseLetterBank.length)]; //Will add upper case if user clicked okay
+    //2  
+    if(lowerCaseLetters == true && upperCaseLetters == false && numericalNumbers == true && specialCharacters == true){
+      combinedPasswordBank = lowerCaseLetterBank.concat(numericalNumbersBank, specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
     }
 
-    if(numericalNumbers == true){
-      randomPasswordGenerated += numericalNumbersBank[Math.floor(Math.random() *numericalNumbersBank.length)]; //Will add numbers if user clicked okay
+    //3
+    if(lowerCaseLetters == true && upperCaseLetters == true && numericalNumbers == false && specialCharacters == true){
+      combinedPasswordBank = lowerCaseLetterBank.concat(upperCaseLetterBank, specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
     }
 
-    if(specialCharacters == true){
-      randomPasswordGenerated += specialCharactersBank[Math.floor(Math.random() * specialCharactersBank.length)]; //Will add special characters if user clicked okay
+    //4
+    if(lowerCaseLetters == true && upperCaseLetters == false && numericalNumbers == false && specialCharacters == true){
+      combinedPasswordBank = lowerCaseLetterBank.concat(specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
     }
 
+    //5
+    if(lowerCaseLetters == true && upperCaseLetters == true && numericalNumbers == true && specialCharacters == false){
+      combinedPasswordBank = lowerCaseLetterBank.concat(upperCaseLetterBank, numericalNumbersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+    //6
+    if(lowerCaseLetters == true && upperCaseLetters == false && numericalNumbers == true && specialCharacters == false){
+      combinedPasswordBank = lowerCaseLetterBank.concat(numericalNumbersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+    
+    //7
+    if(lowerCaseLetters == true && upperCaseLetters == true && numericalNumbers == false && specialCharacters == false){
+      combinedPasswordBank = lowerCaseLetterBank.concat(upperCaseLetterBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+    //8
+    if(lowerCaseLetters == true && upperCaseLetters == false && numericalNumbers == false && specialCharacters == false){
+      combinedPasswordBank = lowerCaseLetterBank;
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+    //9
+    if(lowerCaseLetters == false && upperCaseLetters == true && numericalNumbers == true && specialCharacters == true){
+      combinedPasswordBank = upperCaseLetterBank.concat(numericalNumbersBank, specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+     //10
+    if(lowerCaseLetters == false && upperCaseLetters == false && numericalNumbers == true && specialCharacters == true){
+      combinedPasswordBank = numericalNumbersBank.concat(specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+     //11
+    if(lowerCaseLetters == false && upperCaseLetters == true && numericalNumbers == false && specialCharacters == true){
+      combinedPasswordBank = upperCaseLetterBank.concat(specialCharactersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+     //12
+    if(lowerCaseLetters == false && upperCaseLetters == false && numericalNumbers == false && specialCharacters == true){
+      combinedPasswordBank = specialCharactersBank;
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+     //13
+    if(lowerCaseLetters == false && upperCaseLetters == true && numericalNumbers == true && specialCharacters == false){
+      combinedPasswordBank = upperCaseLetterBank.concat(numericalNumbersBank);
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+     //14
+    if(lowerCaseLetters == false && upperCaseLetters == false && numericalNumbers == true && specialCharacters == false){
+      combinedPasswordBank = numericalNumbersBank;
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+     //15
+    if(lowerCaseLetters == false && upperCaseLetters == true && numericalNumbers == false && specialCharacters == false){
+      combinedPasswordBank = upperCaseLetterBank;
+      randomPasswordGenerated += combinedPasswordBank[Math.floor(Math.random() * combinedPasswordBank.length)]; 
+    }
+
+    //16
     if(lowerCaseLetters == false && upperCaseLetters == false && numericalNumbers == false && specialCharacters == false){
       window.alert("ERROR: Please select yes to at least one to create your password."); //This message will appear if user clicked cancel to everything. 
-
-      //TODO - Need to figure out how to make message go away.
+      key = false; //Set key to false here so that the window alert will close
     }
-
   }
+
+  
 
 
 
